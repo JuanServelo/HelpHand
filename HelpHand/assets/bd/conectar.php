000727671896cd01435cef13d67c42cfbdfe3ecc
Juan Servelo <91964551+JuanServelo@users.noreply.github.com>
2024-04-03 23:04:47 -0300

    Organização dos arquivos
    Organização dos arquivos

--- HelpHand/assets/bd/conectar.php
+++ HelpHand/assets/bd/conectar.php
@@ -7,6 +7,6 @@ global $database;
 // Dados do banco de dados 
 $servername = "localhost:3306";
 $username = "root";
-$password = "PUC@1234";
-$database = "ClinicaA";
+$password = "";
+$database = "HelpHand";
 ?>
