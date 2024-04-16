8b90d88871f0b3e50c0fab56293db811f1ba4e5c
jtancon <101944685+jtancon@users.noreply.github.com>
2024-04-16 19:59:19 -0300

    foi

--- HelpHand/sessao.php
+++ HelpHand/sessao.php
@@ -11,7 +11,7 @@ $P_email = $_POST['email'] ?? NULL;
 $P_senha = $_POST['senha'] ?? NULL;
 
 // Função para verificar o email e senha na tabela usuario do MySQL
-<?php
+function verificarCredenciais($P_email, $P_senha) {
     $servername = "localhost:3306";
     $username = "root";
     $password = "";
@@ -25,27 +25,27 @@ $P_senha = $_POST['senha'] ?? NULL;
     // Consulta SQL para verificar as credenciais
     $sql = "SELECT * FROM usuario WHERE Email = '$P_email'";
     $result = mysqli_query($conn, $sql);
-$servername = "localhost:3306";
-$username = "root";
-$password = "";
-$database = "helphand";
-// Start the session
-$logged = False;
-
-$P_email = $_POST['email'] ?? NULL;
-$P_senha = $_POST['senha'] ?? NULL;
-
-// Função para verificar o email e senha na tabela usuario do MySQL
-function verificarCredenciais($P_email, $P_senha) {
-    $servername = "localhost:3306";
-    $username = "root";
-    $password = "";
-    $database = "helphand";
-    global $servername, $username, $password, $database;
-    $conn = mysqli_connect($servername, $username, $password, $database);
-    if (!$conn) {
-        die("Falha na conexão com o banco de dados: " . mysqli_connect_error());
-    }Fechar a conexão com o banco de dados
-    mysqli_close($conn);
     
-?>    
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        
+        if (isset($row['senha']) && password_verify($P_senha, $row['senha'])) {
+            $logged = True;
+            $_SESSION['logged'] = $logged;
+            $_SESSION['email'] = $P_email;
+            $_SESSION['senha'] = $P_senha;
+            setcookie('biscoito', $P_email, time() + 60 * 60 * 24 * 30);
+            setcookie('tipo', $admin, time() + 60 * 60 * 24 * 30);
+            header('Location: index.php');
+            exit;
+        } else {
+            $_SESSION['erro'] = "Senha Incorreta";
+        }
+    } else {
+        $_SESSION['erro'] = "Email não cadastrado";
+    }    
+
+    // Fechar a conexão com o banco de dados
+    mysqli_close($conn);
+}
+?>
