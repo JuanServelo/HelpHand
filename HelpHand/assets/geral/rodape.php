dd123921db3caa2095194db16cb28cfd857e8bf3
dudatt <dudamelobarbosa@gmail.com>
2024-05-15 15:46:19 -0300

    ajuste no footer e na pág FAQ

--- HelpHand/assets/geral/rodape.php
+++ HelpHand/assets/geral/rodape.php
@@ -4,29 +4,29 @@
 />
 
 <div class="container">
-  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-    <div class="col-md-4 d-flex align-items-center">
-      <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-        <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
-      </a>
-      <span class="mb-3 mb-md-0 text-muted">© 2024 Copyright: HelpHand</span>
-    </div>
+        <footer class="d-flex flex-row justify-content-between py-3 my-4 border-top">
+                <div class="col-md-4 d-flex">
+                <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+                        <svg class="bi" width="30" height="24"><use xlink:href="#bootstrap"></use></svg>
+                </a>
+                <span class="mb-3 mb-md-0 text-muted">© 2024 Copyright: HelpHand</span>
+                </div>
     
-    <div class="container p-4 justify-content-end list-unstyled d-flex">
-            <section class="mb-4">
-                <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" style="background-color: #b3dad4 ; border-radius:50%;" role="button"><i
-                        class="fab fa-facebook-f"></i></a>
-                <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" style="background-color: #b3dad4 ; border-radius:50%;" role="button"><i
-                        class="fab fa-twitter"></i></a>
-                <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" style="background-color: #b3dad4 ; border-radius:50%;" role="button"><i
-                        class="fab fa-google"></i></a>
-                <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" style="background-color: #b3dad4 ; border-radius:50%;" role="button"><i
-                        class="fab fa-instagram"></i></a>
-                <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" style="background-color: #b3dad4 ; border-radius:50%;" role="button"><i
-                        class="fab fa-linkedin-in"></i></a>
-                <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" style="background-color: #b3dad4 ; border-radius:50%;" role="button"><i
-                        class="fab fa-github"></i></a>
-            </section>
-        </div>
-  </footer>
+                <div class="container justify-content-end list-unstyled d-flex">
+                        <section class="mb-4">
+                                <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" style="background-color: #b3dad4 ; border-radius:50%;" role="button"><i
+                                        class="fab fa-facebook-f"></i></a>
+                                <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" style="background-color: #b3dad4 ; border-radius:50%;" role="button"><i
+                                        class="fab fa-twitter"></i></a>
+                                <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" style="background-color: #b3dad4 ; border-radius:50%;" role="button"><i
+                                        class="fab fa-google"></i></a>
+                                <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" style="background-color: #b3dad4 ; border-radius:50%;" role="button"><i
+                                        class="fab fa-instagram"></i></a>
+                                <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" style="background-color: #b3dad4 ; border-radius:50%;" role="button"><i
+                                        class="fab fa-linkedin-in"></i></a>
+                                <a data-mdb-ripple-init class="btn btn-outline btn-floating m-1" href="#!" style="background-color: #b3dad4 ; border-radius:50%;" role="button"><i
+                                        class="fab fa-github"></i></a>
+                        </section>
+                </div>
+        </footer>
 </div>
