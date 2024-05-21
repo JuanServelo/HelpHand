8d1b77584957786d934c879537d39b228d0a20a4
dudatt <dudamelobarbosa@gmail.com>
2024-05-20 22:11:47 -0300

    atualização do front home adm e correção de erros da navbarAdm

--- HelpHand/assets/geral/navbarAdmin_mobile.php
+++ HelpHand/assets/geral/navbarAdmin_mobile.php
@@ -1,10 +1,11 @@
-<div class="modal fade" id="modal_logo_maozinha" tabindex="-1" 
-        aria-labelledby="modal_logo_maozinha" aria-hidden="true"> <!--modal do desktop-->
+<div class="modal top-down-fade" id="modal_menu" tabindex="-1"
+        aria-labelledby="modal_menu" aria-hidden="true"> 
             <div class="modal-dialog modal-fullscreen m-0">
                 <div class="modal-content">
                     <div class="modal-header p-0">
-                        <h1 class="modal-title fs-5 text-center" id="modal_logo_maozinha">
-                            <img src="../assets/img/logo_preta.png" alt="">
+                        <h1 class="modal-title fs-5 text-center" 
+                        id="modal_menu">
+                            <img src="../assets/img/logo_preta.png" width="100%" alt="">
                         </h1>
                         <button type="button" class="btn-close" 
                         data-bs-dismiss="modal" aria-label="Close">
@@ -13,7 +14,7 @@
                     </div>
                     <div class="modal-body p-0">
                         <ol class="d-flex flex-column p-0">
-                          <a href="../administrador/indexAdmin.php" target="_self">
+                        <a href="../administrador/indexAdmin.php" target="_self">
                                 <ion-icon name="home-outline"></ion-icon>
                                 Home
                             </a>
@@ -22,19 +23,20 @@
                                 Gerenciar Usuários
                             </a>
                             <a href="#">
-                                <ion-icon name="settings-outline"></ion-icon>
+                                <ion-icon name="refresh-outline"></ion-icon>
                                 Gerenciar Serviços
                             </a>
                         </ol>
                     </div>     
                     <div class="modal-footer d-flex justify-content-start m-3">
-                        <a href="#" class="profile_picture d-flex justify-content-center align-items-center">
+                        <a class="profile_picture m-0" href="#">
                             <img src="../assets/img/user 1.png" alt="">
                         </a>
-                        <a class="profile_picture-text w-50" href="#">
+                        <a class="profile_picture-text w-75" href="#">
                             <h3>Nome do Usuário</h3>
                         </a>
                     </div>
+
                 </div>
             </div>
         </div>
