afc80b5e4139f24c8c7f86bdec4a961e7fdfce9f
Juan Servelo <91964551+JuanServelo@users.noreply.github.com>
2024-05-21 19:09:52 -0300

    Sessão para colaborador

--- HelpHand/assets/bd/sessao.php
+++ HelpHand/assets/bd/sessao.php
@@ -28,7 +28,7 @@
             unset($_SESSION['conexao']);
         }
 
-        $sql = "SELECT Senha FROM usuario WHERE Email = '$P_email'"; 
+        $sql = "SELECT Senha FROM Usuario WHERE Email = '$P_email'"; 
         $result = mysqli_query($conn, $sql); 
         
                 if (!$result) {
@@ -40,15 +40,53 @@
                 }
                 else{
                     if (mysqli_num_rows($result) == 0) { 
-                        $_SESSION['erro'] = "E-mail não encontrado!"; 
-                        echo "<div class='alert alert-danger' role='alert' style='position: fixed; top: 0; left: 0; width: 100%;'>$_SESSION[erro]</div>";
-                        unset($_SESSION['erro']); 
-                        return; 
+                        $sql = "SELECT Senha FROM Colaborador WHERE Email = '$P_email'"; 
+                        $result = mysqli_query($conn, $sql); 
+                        
+                        if (!$result) {
+                            $_SESSION['conexao'] = mysqli_error($conn);
+                            $_SESSION['erro'] = "Erro na consulta: " + $_SESSION['conexao'];
+                            echo "<div class='alert alert-danger' role='alert'>$_SESSION[erro]</div>";
+                            unset($_SESSION['erro']);
+                            unset($_SESSION['conexao']);
+                        }
+                        else{
+                            if (mysqli_num_rows($result) == 0) { 
+                            $_SESSION['erro'] = "E-mail não encontrado!"; 
+                            echo "<div class='alert alert-danger' role='alert' style='position: fixed; top: 0; left: 0; width: 100%;'>$_SESSION[erro]</div>";
+                            unset($_SESSION['erro']); 
+                            return;
+                            }
+                            $row = mysqli_fetch_row($result);
+                            $senha_do_bd = $row[0];
+                            if (password_verify($P_senha, $senha_do_bd)) {
+                                $_SESSION['logged'] = True;
+                                $_SESSION['email'] = $P_email;
+                                setcookie('biscoito', $P_email, time() + 60 * 60 * 24 * 30);
+                                $nome_user = "SELECT Nome FROM Colaborador WHERE Email = '$P_email'";
+                                $consultaNome = mysqli_query($conn, $nome_user);
+                                $colunaNome = mysqli_fetch_row($consultaNome);
+                                $_SESSION['nome'] = $colunaNome[0];
+                                if (basename($_SERVER['PHP_SELF']) == 'login.php') {
+                                    header('Location: autonomo/index.php');
+                                    exit;
+                                } else {
+                                    header('Location: ../autonomo/index.php');
+                                    exit;
+                                }   
+                                
+                            } else {
+                                $_SESSION['erro'] = "Senha incorreta!";
+                                echo "<div class='alert alert-danger' role='alert'>$_SESSION[erro]</div>";
+                                unset($_SESSION['erro']);
+                            }
+                        }
                     }
                     $row = mysqli_fetch_row($result);
                     $senha_do_bd = $row[0]; 
                     if (password_verify($P_senha, $senha_do_bd)) {
                         $_SESSION['logged'] = True;
+                        $_SESSION['email'] = $P_email;
                         setcookie('biscoito', $P_email, time() + 60 * 60 * 24 * 30);
                         $nome_user = "SELECT Nome FROM usuario WHERE Email = '$P_email'";
                         $consultaNome = mysqli_query($conn, $nome_user);
