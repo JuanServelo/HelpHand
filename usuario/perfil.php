cfb04c54c760fcb859faa86e790954adc817d02d
EricBinek <ericbineksant@gmail.com>
2024-05-22 19:40:53 -0300

    User

--- usuario/perfil.php
+++ usuario/perfil.php
@@ -3,7 +3,7 @@ include_once '../assets/bd/sessao.php';
 $email = $_SESSION['email'];
 
 
-$sql = "SELECT Nome, Telefone, Email, CPF, Foto, Genero FROM Colaborador WHERE Email = ?";
+$sql = "SELECT Nome, Telefone, Email, CPF, Foto, Genero FROM Usuario WHERE Email = ?";
 
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("i", $email);
@@ -27,10 +27,10 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="perfilc.css">
+    <link rel="stylesheet" href="perfil.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-CzPQD8+6b7Hd30GQ2pPCbLK/20c5vB5F0lZMR+6z4QhoV6eHA/p0B5G94Byjz93B" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap">
-    <title>Perfil Colaborador</title>
+    <title>Perfil</title>
 </head>
 <body>
     <div class="container">
